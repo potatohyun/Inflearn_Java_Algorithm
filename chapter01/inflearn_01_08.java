c0e82ed54577f1266d7859fbd997adb1f4d87b8d
potatohyun <devhyun08>
2024-08-22 01:12:29 +0900

    01-08

---
+++ chapter01/inflearn_01_08.java
@@ -0,0 +1,60 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class inflearn_01_08 {
+    public static void main(String[] args){
+        inflearn_01_08 T = new inflearn_01_08();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
+    }
+
+    public String solution(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            if (!Character.isAlphabetic(str.charAt(start))) start++;
+            else if (!Character.isAlphabetic(str.charAt(end))) end--;
+            else {
+                if (Character.toLowerCase(str.charAt(start)) != Character.toLowerCase(str.charAt(end))) return "NO";
+                start++;
+                end--;
+            }
+        }
+        return "YES";
+    }
+
+    public String lectureSolution(String str) {
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]",""); // 알파벳을 제외하고는 지워주는 작업
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) answer = "YES";
+        return answer;
+    }
+}
+
+
+/*설명
+앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+
+문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+
+단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
+
+알파벳 이외의 문자들의 무시합니다.
+
+입력
+첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다.
+
+출력
+첫 번째 줄에 팰린드롬인지의 결과를 YES 또는 NO로 출력합니다.
+
+예시 입력 1
+found7, time: study; Yduts; emit, 7Dnuof
+
+예시 출력 1
+YES
+
+ */
+
